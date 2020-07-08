ab3d8f57279095986ade8542eaa60321109ffe85
fengdeguisu <fanzhou_0808@163.com>
2020-07-08 23:53:36 +0800

    update demo_branch1 add Hello.java and add User.java

--- Hello.java
+++ Hello.java
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 public class Hello{
-	
 	public static void main(String[] args){
 		System.out.println(1);
 		System.out.println("Hello World !");
 		System.out.println(2);
 		System.out.println("Hello World !");
-=======
-public class HelloWorld{
-	
-	public static void main(String[] args){
-		System.out.println("hello world !");
-		System.out.println(1);
-		System.out.println("Hello World !");
-		System.out.println(2);
->>>>>>> 88119a359b986b057091ccf27ecd480670057a0d
 	}
 }
--- User.java
+++ User.java
@@ -1,10 +1,10 @@
-public class User2{
+public class User{
 	private String name;
 	private int age;
 	
-	public User2(){}
+	public User(){}
 	
-	public User2(String name,int age){
+	public User(String name,int age){
 		this.name = name;
 		this.age = age;
 	}
